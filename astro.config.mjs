0120b95547cc66b4c2bfe607964b6952d8be2a6c
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2024-09-30 13:00:47 -0500

    remove @astrojs/cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import svelte from '@astrojs/svelte';
-// import  cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({

21a908ff6923b24c5b08be6b478f67b6c0514332
AarynSmith <4387096+AarynSmith@users.noreply.github.com>
2024-10-01 12:41:26 -0500

    update theme

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
+const espressoDark = '#2C1A15';
+const creamyBeige = '#F5E6D3';
+const carmelTan = '#C69C6D';
+const richCoffeeBrown = '#3B2A20';
+const mochaGray = "#B0A99F";
+
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			"surface-l": creamyBeige,
+			"primary-l": richCoffeeBrown,
+			"secondary-l": mochaGray,
+			"headers-l": espressoDark,
+			"links-l": carmelTan,
+			"accent-l": carmelTan,
+			"surface-d": espressoDark,
+			"primary-d": creamyBeige,
+			'secondary-d':richCoffeeBrown,
+			'headers-d':  carmelTan,
+			'links-d': carmelTan,
+			'accent-d': mochaGray,
+		}
 	},
 	plugins: [],
 }
